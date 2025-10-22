ce21a43386e553133bf00187e5172cd68e9b267e
fabiooc1 <fabiocaldas266@gmail.com>
2025-10-22 13:15:02 -0300

    removendo responsabilidade de navegar para pagina de login

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,7 +1,7 @@
 import type { UserModel } from "@/models/UserModel";
 import { userService } from "@/services/userService";
 import { createContext, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+
 type AuthContextProps = {
     user: UserModel | null;
     isLoadingAuth: boolean;
@@ -13,14 +13,12 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     const [user, setUser] = useState<UserModel | null>(null)
     const [isLoadingAuth, setIsLoadingAuth] = useState<boolean>(true)
 
-    const navigate = useNavigate()
-
     async function loadUser() {
         try {
             const userData = await userService.get()
             setUser(userData)
         } catch (error: any) {
-            navigate('/login')
+            setUser(null)
         } finally {
             setIsLoadingAuth(false)
         }
