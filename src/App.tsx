f29e5dd0ec0df0b9fce873590b8dc3a60a74583a
fabiooc1 <fabiocaldas266@gmail.com>
2025-11-06 20:36:48 -0300

    melhorando layout de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Toaster } from "./components/ui/sonner";
+import { AuthProvider } from "./contexts/AuthContext";
 import { RequireAuth } from "./components/auth/RequireAuth";
 import { LoginPage } from "./pages/LoginPage";
-import { RootLayout } from "./pages/RootLayout";
-import { AuthProvider } from "./contexts/AuthContext";
-import { FerryPage } from "./pages/FerryPage";
+import { PanelLayout } from "./pages/PanelLayout";
+import { ViagensPage } from "./pages/ViagensPage";
 
 function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
         <Routes>
-          <Route element={<RootLayout />}>
-            <Route path="/login" element={<LoginPage />} />
-
-            <Route element={<RequireAuth />}>
-              <Route path="/dashboard" element={<div>Dashboard page</div>} />
-              <Route path="/ferry" element={<FerryPage />} />
+          <Route element={<RequireAuth />}>
+            <Route element={<PanelLayout />}>
+              <Route path="/" element={<h1>Painel</h1>} />
+              <Route path="/viagens" element={<ViagensPage />} />
             </Route>
           </Route>
+
+          <Route path="/auth/login" element={<LoginPage />} />
         </Routes>
       </AuthProvider>
 
