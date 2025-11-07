349774e8a072bc26d8a8d3762e7ae70ab4e81b45
fabiooc1 <fabiocaldas266@gmail.com>
2025-11-06 21:15:23 -0300

    pagina de viagem

---
+++ src/pages/ViagensPage/index.tsx
@@ -0,0 +1,62 @@
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupInput,
+} from "@/components/ui/input-group";
+import { ViagensListing } from "./components/ViagensListing";
+import { PlusCircle, Search } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { FormDialog } from "@/components/FormDialog";
+import { RegisterTripForm } from "./components/RegisterTripForm";
+import { useTrips } from "@/hooks/useTrips";
+
+export function ViagensPage() {
+  const { tripsPagination, isLoading, loadTrips } = useTrips();
+
+  if (isLoading) {
+    return "carregando..."
+  }
+
+  return (
+    <>
+      <div className="space-y-6">
+        <div>
+          <h1 className="font-bold text-2xl">Viagens</h1>
+          <p>Cadastre, delete ou edite as viagens de ferry.</p>
+        </div>
+
+        <div className="space-y-4">
+          <div className="flex items-center justify-between">
+            <form className="flex items-start gap-2">
+              <InputGroup>
+                <InputGroupAddon>
+                  <Search />
+                </InputGroupAddon>
+
+                <InputGroupInput placeholder="Rota" />
+              </InputGroup>
+
+              <Button variant="secondary">Pesquisar</Button>
+            </form>
+
+            <FormDialog
+              title="Cadastrar viagem"
+              description="Cadastre uma nova viagem de ferry"
+              trigger={
+                <Button>
+                  <PlusCircle />
+                  Cadastrar viagem
+                </Button>
+              }
+              onSuccess={() => loadTrips()}
+            >
+              {(_, onSuccess) => <RegisterTripForm onSuccess={onSuccess} />}
+            </FormDialog>
+          </div>
+
+          {tripsPagination?.data && <ViagensListing viagens={tripsPagination.data} />}
+        </div>
+      </div>
+    </>
+  );
+}
